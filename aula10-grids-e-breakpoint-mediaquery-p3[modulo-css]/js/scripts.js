5bbbf95748fbce3736e5f08907deea48ef6be719
Wagner Lima <80631657+wagnerlimanet@users.noreply.github.com>
2025-06-23 19:57:50 -0300

    Aula 10 - Grids e Breakpoints [Parte 03]
    Conteúdo ministrado nas aulas de CSS, BOOTSTRAP na turma FDEV012

---
+++ aula10-grids-e-breakpoint-mediaquery-p3[modulo-css]/js/scripts.js
@@ -0,0 +1,19 @@
+//  » Wagner Lima | www.wagnerlima.net | @wagnerlimaNET «
+//  Professor de desenvolvimento web e design na escola iwtraining, especialista em mecanismos de
+//  buscas (SEO) e graduando em Sistemas e Mídias Digitais na Universidade Federal do Ceará (UFC).
+
+// início dos sons dos animais //
+function abelha() {
+    var abelha = new Audio();
+    abelha.src = "../sons/abelha-bee.mp3";
+    abelha.play();
+    }
+
+function alce() {
+    var alce = new Audio();
+    alce.src = "../sons/alce-moose.mp3";
+    alce.play();
+    }
+
+    
+// fim dos sons dos animais //
